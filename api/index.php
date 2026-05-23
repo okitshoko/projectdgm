feae103f7e67593979fcf7d479e19e6b2eb540a9
OKIT SHOKO <okitshoko@gmail.com>
2026-05-23 13:33:44 +0200

    Nettoyage du point d'entree api

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+// 1. Charger l'autoloader de Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Démarrer l'application Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 3. Exécuter le point d'entrée public
 require __DIR__ . '/../public/index.php';
